99a87e0ef5214697321f92abdd8cb9b174d72349
RobertHESSO <robert.lacic@students.hevs.ch>
2021-04-04 09:08:44 +0200

    MainMenu hinzugefügt

---
+++ app/src/main/java/com/example/hotelmanagement/MenuActivity.java
@@ -0,0 +1,45 @@
+package com.example.hotelmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button1;
+    private Button button2;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+        button1 = (Button) findViewById(R.id.RoomButton);
+        button1.setOnClickListener(this);
+
+        button2 = (Button) findViewById(R.id.ReservationButton);
+        button2.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.RoomButton:
+                startActivity(new Intent(this, RoomListActivity.class));
+                break;
+            case R.id.ReservationButton:
+                startActivity(new Intent(this, ReservationListActivity.class));
+                break;
+
+        }
+    }
+}
